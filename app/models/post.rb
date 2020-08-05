ddf5f35653854f6438bf44b71df3c85f07079b0d
Chloe Steffens <caforonda103@yahoo.com>
2020-08-05 16:11:30 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ActiveRecord::Base
+<<<<<<< HEAD
   
   def post_summary
     self.title + " - " + self.description
   end
   
+=======
+>>>>>>> 7c2ba9846271309e6bbbd15770281a0818aca095
 end
